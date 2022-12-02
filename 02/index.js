b26f03b7cdf4552ce45a0c8c3767e1f276b0d223
Riccardo Zaffalon <rzaffalon@alpenite.com>
2022-12-02 10:17:02 +0100

    Add day 2 part 2

--- 02/index.js
+++ 02/index.js
@@ -1,7 +1,13 @@
 
 import { getInputString, sum } from '../utils.js';
 
-const PLAYER_MAP = ['X', 'Y', 'Z'];
+const PLAYER_MAP = new Map();
+
+PLAYER_MAP.set('rock', 'X');
+PLAYER_MAP.set('paper', 'Y');
+PLAYER_MAP.set('scissors', 'Z');
+
+const PLAYER_LIST = ['X', 'Y', 'Z'];
 
 function play(opponent, player) {
     let result = 0;
@@ -52,19 +58,77 @@ function play(opponent, player) {
             break;
     }
 
-    return result + PLAYER_MAP.indexOf(player) + 1;
+    return result + PLAYER_LIST.indexOf(player) + 1;
+}
+
+function choose(opponent, result) {
+    switch (opponent) {
+        case 'A': // Rock
+            if (result === 'X') { // Lose
+                return PLAYER_MAP.get('scissors');
+            }
+
+            if (result === 'Y') { // Draw
+                return PLAYER_MAP.get('rock');
+            }
+
+            if (result === 'Z') { // Win
+                return PLAYER_MAP.get('paper');
+            }
+            break;
+    
+        case 'B': // Paper
+            if (result === 'X') { // Lose
+                return PLAYER_MAP.get('rock');
+            }
+
+            if (result === 'Y') { // Draw
+                return PLAYER_MAP.get('paper');
+            }
+
+            if (result === 'Z') { // Win
+                return PLAYER_MAP.get('scissors');
+            }
+            break;
+    
+        case 'C': // Scissors
+            if (result === 'X') { // Lose
+                return PLAYER_MAP.get('paper');
+            }
+
+            if (result === 'Y') { // Draw
+                return PLAYER_MAP.get('scissors');
+            }
+
+            if (result === 'Z') { // Win
+                return PLAYER_MAP.get('rock');
+            }
+            break;
+    
+        default:
+            break;
+    }
 }
 
 const input = getInputString('./02/input.txt');
 
 const matches = input.split('\n')
 
-const results = matches.map((match) => {
+const resultsOne = matches.map((match) => {
     const [opponent, player] = match.split(' ');
 
     return play(opponent, player);
 });
 
-const score = sum(results);
+const resultsTwo = matches.map((match) => {
+    const [opponent, result] = match.split(' ');
+    const player = choose(opponent, result);
 
-console.log('Score: ', score);
+    return play(opponent, player);
+});
+
+const scoreOne = sum(resultsOne);
+const scoreTwo = sum(resultsTwo);
+
+console.log('Score Part 1: ', scoreOne);
+console.log('Score Part 2: ', scoreTwo);
