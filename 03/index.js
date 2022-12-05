b48d402f0233d1261c798e3a263be732090af1f3
Riccardo Zaffalon <rzaffalon@alpenite.com>
2022-12-05 10:00:05 +0100

    Add day 3 part 2

--- 03/index.js
+++ 03/index.js
@@ -1,5 +1,7 @@
 import { getInputString, Alphabet, sum } from '../utils.js';
 
+const PRIORITIES = Alphabet.all;
+
 // const input = `vJrwpWtwJgWrhcsFMMfFFhFp
 // jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
 // PmmdzqPrVvPwwTWBwg
@@ -21,12 +23,33 @@ const types = sacks.map((sack) => {
     return a.find((letter) => b.includes(letter));
 });
 
-const priorities = Alphabet.all;
+let groups = [];
 
-const result = sum(
-    types.map((letter) => {
-        return priorities.indexOf(letter) + 1;
-    })
-);
+sacks.forEach((sack, index) => {
+    const i = Math.floor(index / 3);
+    const target = groups[i] || [];
+    const group = [...target, sack];
+    groups[i] = group;
+});
 
-console.log(result);
+const badges = groups.map((groupSacks) => {
+    const a = groupSacks[0].split('');
+    const b = groupSacks[1].split('');
+    const c = groupSacks[2].split('');
+
+    return a.find((letter) => b.includes(letter) && c.includes(letter));
+});
+
+function solve(inputs) {
+    return sum(
+        inputs.map((letter) => {
+            return PRIORITIES.indexOf(letter) + 1;
+        })
+    );
+}
+
+const resultA = solve(types);
+const resultB = solve(badges);
+
+console.log(resultA);
+console.log(resultB);
