85a46921cb538d77f09b396243b79ff8e2386b3b
Riccardo Zaffalon <rzaffalon@alpenite.com>
2022-12-07 10:53:30 +0100

    Add day 4 part 2

--- 04/index.ts
+++ 04/index.ts
@@ -8,6 +8,22 @@ type Range = {
     max: number;
 };
 
+function getRanges(couple: string) {
+    const [itemA, itemB] = couple.split(',');
+
+    const a: Range = {
+        min: parseInt(itemA.split('-')[0]),
+        max: parseInt(itemA.split('-')[1]),
+    };
+
+    const b: Range = {
+        min: parseInt(itemB.split('-')[0]),
+        max: parseInt(itemB.split('-')[1]),
+    };
+
+    return [a, b];
+}
+
 function isOneRangeFullyContained(range: Range[]) {
     const [a, b] = range;
 
@@ -22,20 +38,31 @@ function isOneRangeFullyContained(range: Range[]) {
     return false;
 }
 
+function doRangesOverlap(range: Range[]) {
+    const [a, b] = range;
+
+    if (a.min >= b.min && a.min <= b.max) {
+        return true;
+    }
+
+    if (b.min >= a.min && b.min <= a.max) {
+        return true;
+    }
+
+    return false;
+}
+
 const full = couples.filter((couple) => {
-    const [itemA, itemB] = couple.split(',');
-
-    const a: Range = {
-        min: parseInt(itemA.split('-')[0]),
-        max: parseInt(itemA.split('-')[1]),
-    };
-
-    const b: Range = {
-        min: parseInt(itemB.split('-')[0]),
-        max: parseInt(itemB.split('-')[1]),
-    };
+    const [a, b] = getRanges(couple);
 
     return isOneRangeFullyContained([a, b]);
 });
 
-console.log(full.length);
+const overlap = couples.filter((couple) => {
+    const [a, b] = getRanges(couple);
+
+    return doRangesOverlap([a, b]);
+});
+
+console.log('Result Part 1: ', full.length);
+console.log('Result Part 2: ', overlap.length);
