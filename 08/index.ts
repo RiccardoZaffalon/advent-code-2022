14c9e2e5f85338469661a35c821adbd971435241
Riccardo Zaffalon <riccardo.zaffalon@gmail.com>
2022-12-12 21:54:38 +0100

    Add day 8 part 1

--- 08/index.ts
+++ 08/index.ts
@@ -1,53 +1,8 @@
-const input = Deno.readTextFileSync('./08/sample.txt');
-// const input = Deno.readTextFileSync('./08/input.txt');
+// const input = Deno.readTextFileSync('./08/sample.txt');
+const input = Deno.readTextFileSync('./08/input.txt');
 
 const rows = input.split('\n');
-const columns = [];
-
-function invert(array: string[]) {
-    return array.map((trees) => {
-        return trees.split('').reverse().join('');
-    });
-}
-
-// for (let i = 0; i < rows.length; i++) {
-//     const row = rows[i];
-//     const trees = row.split('');
-
-//     for (let j = 0; j < trees.length; j++) {
-//         const tree = trees[j];
-
-//         if (columns[j]) {
-//             columns[j] += tree;
-//         } else {
-//             columns[j] = tree;
-//         }
-//     }
-// }
-
-// const rows_inverted = invert(rows);
-// const columns_inverted = invert(columns);
-
-// console.log({
-//     rows,
-//     rows_inverted,
-//     columns,
-//     columns_inverted,
-// });
-
-function canBeSeenFrom(row: number, column: number, side: string) {
-    const value = rows[row].charAt(column);
-
-    switch (side) {
-        case 'left':
-            return value;
-            break;
-        default:
-            break;
-    }
-
-    return false;
-}
+const columns: string[] = [];
 
 for (let i = 0; i < rows.length; i++) {
     const row = rows[i];
@@ -56,6 +11,71 @@ for (let i = 0; i < rows.length; i++) {
     for (let j = 0; j < trees.length; j++) {
         const tree = trees[j];
 
-        console.log(canBeSeenFrom(i, j, 'left'));
+        if (columns[j]) {
+            columns[j] += tree;
+        } else {
+            columns[j] = tree;
+        }
     }
 }
+
+type Side = 'top' | 'right' | 'bottom' | 'left';
+function canBeSeenFrom(side: Side, row: number, column: number, value: number) {
+    switch (side) {
+        case 'left': {
+            const trees = rows[row].split('').filter((_el: string, i: number) => i < column);
+            if (!trees.length) return true;
+
+            return trees.every((tree) => parseInt(tree) < value);
+        }
+
+        case 'right': {
+            const trees = rows[row].split('').filter((_el: string, i: number) => i > column);
+            if (!trees.length) return true;
+
+            return trees.every((tree) => parseInt(tree) < value);
+        }
+
+        case 'top': {
+            const trees = columns[column].split('').filter((_el: string, i: number) => i < row);
+
+            if (!trees.length) return true;
+
+            return trees.every((tree) => parseInt(tree) < value);
+        }
+
+        case 'bottom': {
+            const trees = columns[column].split('').filter((_el: string, i: number) => i > row);
+
+            if (!trees.length) return true;
+
+            return trees.every((tree) => parseInt(tree) < value);
+        }
+
+        default:
+            return false;
+    }
+}
+
+let count = 0;
+
+for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+    const columns = row.split('');
+
+    for (let j = 0; j < columns.length; j++) {
+        const tree = parseInt(columns[j]);
+
+        const isVisible =
+            canBeSeenFrom('left', i, j, tree) ||
+            canBeSeenFrom('right', i, j, tree) ||
+            canBeSeenFrom('top', i, j, tree) ||
+            canBeSeenFrom('bottom', i, j, tree);
+
+        if (isVisible) {
+            count++;
+        }
+    }
+}
+
+console.log('Solution to part 1 is: ', count);
